e7551be6c73a508e22b5007810f99ced7e0bec62
Denis Roussel <denis.roussel@acsone.eu>
2025-02-13 11:39:17 +0100

    [IMP] mail_activity_team: Upgrade module to Beta

--- mail_activity_team/__manifest__.py
+++ mail_activity_team/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mail Activity Team",
     "summary": "Add Teams to Activities",
     "version": "16.0.1.0.1",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "ForgeFlow, Sodexis, Odoo Community Association (OCA)",
